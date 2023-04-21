478704be254a76e41f1e5bdf8238db33e8e69a14
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-21 11:35:39 +0200

    Format code

--- cmd/simple/main.go
+++ cmd/simple/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    "github.com/xyproto/simplejwt"
+	"github.com/xyproto/simplejwt"
 )
 
 func main() {
-    // Set the JWT secret
-    simplejwt.SetSecret("your-secret-key")
+	// Set the JWT secret
+	simplejwt.SetSecret("your-secret-key")
 
-    // Generate a token
-    payload := simplejwt.Payload{
-        Sub: "1234567890",
-        Exp: time.Now().Add(time.Hour).Unix(),
-    }
+	// Generate a token
+	payload := simplejwt.Payload{
+		Sub: "1234567890",
+		Exp: time.Now().Add(time.Hour).Unix(),
+	}
 
-    token, err := simplejwt.Generate(payload, nil)
-    if err != nil {
-        fmt.Printf("Failed to generate token: %v\n", err)
-        return
-    }
+	token, err := simplejwt.Generate(payload, nil)
+	if err != nil {
+		fmt.Printf("Failed to generate token: %v\n", err)
+		return
+	}
 
-    fmt.Printf("Generated token: %s\n", token)
+	fmt.Printf("Generated token: %s\n", token)
 
-    // Validate the token
-    decodedPayload, err := simplejwt.Validate(token)
-    if err != nil {
-        fmt.Printf("Failed to validate token: %v\n", err)
-        return
-    }
+	// Validate the token
+	decodedPayload, err := simplejwt.Validate(token)
+	if err != nil {
+		fmt.Printf("Failed to validate token: %v\n", err)
+		return
+	}
 
-    fmt.Printf("Decoded payload: %+v\n", decodedPayload)
+	fmt.Printf("Decoded payload: %+v\n", decodedPayload)
 }
