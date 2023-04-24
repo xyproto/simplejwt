ed767373e13eed000ed8a29a662fcaabe385a7c9
Alexander F. Rødseth <rodseth@gmail.com>
2023-04-24 21:44:43 +0200

    Add another example

---
+++ cmd/simpler/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xyproto/simplejwt"
+)
+
+func main() {
+	// Set the secret that is used for generating and validating JWT tokens
+	simplejwt.SetSecret("hunter1")
+
+	// Generate a token
+	token := simplejwt.SimpleGenerate("bob@zombo.com", 3600)
+	if token == "" {
+		fmt.Println("Failed to generate token")
+		return
+	}
+	fmt.Printf("Generated token: %s\n", token)
+
+	// Validate the token
+	decodedSubject := simplejwt.SimpleValidate(token)
+	if decodedSubject == "" {
+		fmt.Println("Failed to validate token")
+		return
+	}
+	fmt.Printf("Decoded payload, got subject: %s\n", decodedSubject)
+}
